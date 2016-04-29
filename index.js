1ffa898aac14270825d773c3da9d3bb3f67ee131
Sujit, Jefree <SujitJ@advisory.com>
2016-04-29 19:18:06 +0530

    time-formatter v1.0

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 'use strict';
 
-var formatTime = function(value, format) {
-	if (typeof value === 'number' && value >= 0) {
-		if(format === 'minutes') {
-			if (value < 60 ) {
-				return value+" min";
-			} else {
-				var minutes = value%60,
-					hours = value/60;
-				return hours+" hr "+minutes+" min";
-			}
+var validate = function (value, type) {
+	if (value < 0 || typeof value !== "number" || typeof type !== "boolean") {
+		return false
+	} else {
+		return true
+	}
+}
+
+var formatTime = function() {};
+
+formatTime.prototype.minutes = function(value, type) {
+	if(validate(value,type)) {
+		if (value < 60 || type === true) {
+			return value+" min ";
+		} else {
+			var minutes = value%60,
+				hours = formatTime.prototype.hours(Math.floor(value/60), 'complete');
+			return hours+minutes+" min ";
 		}
 	}
 };
 
-module.exports = formatTime;
+formatTime.prototype.seconds = function(value, type) {
+	if(validate(value,type)) {
+		if (value < 60 || type === true) {
+			return value+" sec ";
+		} else {
+			var seconds = value%60,
+				minutes = formatTime.prototype.minutes(Math.floor(value/60), 'complete');
+			return minutes+seconds+" sec ";
+		}
+	}
+};
+
+formatTime.prototype.hours = function(value, type) {
+	if(validate(value,type)) {
+		if (value < 24 || type === true) {
+			return value+" hr ";
+		} else {
+			var hours = value%24,
+				days = formatTime.prototype.days(Math.floor(value/24), 'complete');
+			return days+hours+" hr ";
+		}
+	}	
+};
+
+formatTime.prototype.days = function(value, type) {
+	return value+" day ";
+};
+
+module.exports = new formatTime();
